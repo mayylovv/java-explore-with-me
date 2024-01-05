649b08dc61b3e146c2767fb197276cfb3077577b
mayylovv <119136126+mayylovv@users.noreply.github.com>
2024-01-06 01:50:02 +0300

    Дипломная работа коммит 1.1

--- ewm-stats-service/server/src/main/java/ru/practicum/model/StatSvc.java
+++ ewm-stats-service/server/src/main/java/ru/practicum/model/StatSvc.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Data
